64c159b201e3cdfad37167bbfc9da5d61e06a24c
Kyle Cheung <kcheung2@ucvts.tec.nj.us>
2019-11-19 08:55:16 -0500

    tutorial commit

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,10 +1,58 @@
 public class BankAccount {
         
+	private static long prevAccountNo = 100000000L;
+	
     private int pin;
     private long accountNo;
     private double balance;
     private User accountHolder;
     
+    public BankAccount(int pin, long accountNo, double balance, User accountHolder) {
+        this.pin = pin;
+        this.accountNo = ++BankAccount.prevAccountNo;
+        this.balance = 0.0;
+        this.accountHolder = accountHolder;
+    }
+    
+    public int getPin() {
+        return pin;
+    }
+    
+    public long getAccountNo() {
+        return accountNo;
+    }
+    
+    public double getBalance() {
+    	NumberFormat currency = NumberFormat.getCurrencyInstance();
+        
+        return currency.format(balance);
+    }
+    
+    public User getAccountHolder() {
+        return accountHolder;
+    }
+    
+    public void deposit(double amount) {
+    	if (amount <= 0) {
+            return ATM.INVALID;    
+        } else {
+            balance = balance + amount;
+        }
+            
+        return ATM.SUCCESS;
+    }
+    
+    public void withdraw(double amount) {
+    	if (amount <= 0) {
+            return ATM.INVALID;
+        } else if (amount > balance) {
+            return ATM.INSUFFICIENT;
+        } else {
+            balance = balance - amount;
+        }
+        
+        return ATM.SUCCESS;
+    }
     ////////////////////////////////////////////////////////////////////////////
     //                                                                        //
     // Refer to the Simple ATM tutorial to fill in the details of this class. //
