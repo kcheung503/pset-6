45404777b7019d4a7e578d5082b1d2da219e1e36
Kyle Cheung <kcheung2@ucvts.tec.nj.us>
2019-11-25 09:10:41 -0500

    fixed user.java

--- src/User.java
+++ src/User.java
@@ -1,17 +1,17 @@
 public class User {
-    
+
     private String firstName;
     private String lastName;
-    
+
     public User(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
     }
-    
+
     public String getFirstName() {
         return firstName;
     }
-    
+
     public String getLastName() {
         return lastName;
     }
@@ -21,16 +21,15 @@ public class User {
     // Refer to the Simple ATM tutorial to fill in the details of this class. //
     //                                                                        //
     ////////////////////////////////////////////////////////////////////////////
-    
+
     /**
      * Formats the first and last name in preparation to be written to the data file.
-     * 
+     *
      * @return a fixed-width string in line with the data file specifications.
      */
-    
+
     public String serialize() {
         return String.format("%1$-" + ATM.FIRST_NAME_WIDTH + "s", firstName) +
             String.format("%1$-" + ATM.LAST_NAME_WIDTH + "s", lastName);
     }
 }
-
